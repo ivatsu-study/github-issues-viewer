7a1613e042db7a8e66bada78485bdf74dc99d56b
IvaTsu <ivan.tsud@1337.tech>
2022-09-06 16:51:17 +0200

    feat: implement api get method 'getComments'

--- src/api/githubAPI.tsx
+++ src/api/githubAPI.tsx
@@ -1,3 +1,20 @@
 import axios from 'axios';
 import parseLink, { Links } from 'parse-link-header';
 
+export interface User {
+  login: string
+  avatar_url: string
+}
+
+export interface Comment {
+  id: number
+  body: string
+  user: User
+  created_at: string
+  updated_at: string
+}
+
+export async function getComments(url: string) {
+  const { data } = await axios.get<Comment[]>(url);
+  return data;
+}
